6294fdab4f0f31cc8f64da756da53da60d1262dc
Yury Bendersky <taas@bisguard.com>
2017-01-26 13:41:50 +0200

    Add files via upload

---
+++ test/test.bat
@@ -0,0 +1,3 @@
+javac -d bin -sourcepath src -cp lib/httpasyncclient- 4.1.2.jar;lib/httpclient-4.5.2.jar;lib/commons-cli-1.3.1.jar;lib/commons-logging-1.2.jar;lib/httpcore-nio-4.4.5.jar;lib/httpcore-4.4.5.jar;lib/commons-csv-1.4.jar;lib/httpclient-4.5.2.jar;lib/org.json.jar src/Test.java
+
+java -cp bin;lib/httpasyncclient-4.1.2.jar;lib/httpclient-4.5.2.jar;lib/commons-cli-1.3.1.jar;lib/commons-logging-1.2.jar;lib/httpcore-nio-4.4.5.jar;lib/httpcore-4.4.5.jar;lib/commons-csv-1.4.jar;lib/httpclient-4.5.2.jar;lib/org.json.jar Test
